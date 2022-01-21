5e1d7a8c5c84df38b58898d7e797e1451eac864a
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2022-01-21 13:08:12 +0200

    Initial commit

---
+++ try.py
@@ -0,0 +1,31 @@
+
+n = 4
+print(sum(range(n+1)))
+
+a = list(range(1, 6))
+print(a)
+print(sum(a))
+
+a = 0
+for i in range(1, 6):
+    a += i
+print(a)
+
+array = [1, 2, 3]
+print(array)
+
+matrix = [[1, 2, 3],
+          [4, 5, 6]]
+
+print(matrix)
+print(matrix[0])
+print(matrix[1])
+print(matrix[0][1])
+
+
+# for x in range(1, 4):
+#     print(x)
+# sum on range(1, 3)
+#     print
+
+# sum(range(102, 2001, 3))
